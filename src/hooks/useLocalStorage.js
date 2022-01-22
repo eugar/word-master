7f15b98bc0a4f5b38e1aff75ff634e9cabce6245
Eugene <9966170+eugar@users.noreply.github.com>
2022-01-22 14:22:40 -0700

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -6,7 +6,6 @@ export const useLocalStorage = (key, initialValue) => {
       const item = window.localStorage.getItem(key)
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
-      console.log(error)
       return initialValue
     }
   })
